2888d295fd1a645951bc65dbaa78eeb230285c4f
Vitor Rodarte Ricoy <vitor.ricoy@dtidigital.com.br>
2019-05-18 22:42:23 -0300

    Atribuicao das funcoes

--- TP.h
+++ TP.h
@@ -23,48 +23,63 @@ enum Direcao {
 };
 
 //Detecta o tamanho do terminal
+//Vitor
 TamanhoTela detectarTamanhoTela();
 
 //Move o cursor da tela para a posicao especificada
+//Vitor
 void moverCursorTela(Coordenada coordenada);
 
 //Limpa a tela
+//Gabriel
 void limparTela();
 
 //Detecta uma tecla pressionada no teclado
+//Vitor
 char detectarTeclado();
 
 //Verifica a morte da cobra
+//Gabriel
 int verificarMorte(Coordenada[] cobra, TamanhoTela tamanhoTela);
 
 //Verifica se a comida foi comida
+//Vitor
 int verificarComida(Coordenada[] cobra, Coordenada comida);
 
 //Verifica se o movimento pressionado é valido
+//Vitor
 int verificarMovimento(Direcao direcaoAtual, Direcao novaDirecao);
 
 //Gera uma nova comida
+//Gabriel
 Coordenada gerarNovaComida(TamanhoTela tamanhoTela, Coordenada[] cobra);
 
 //Movimenta a cobra na direcao determinada
+//Gabriel
 void movimentarCobra(Coordenada[] cobra, Direcao direcao);
 
 //Apaga a cobra antiga
+//Vitor
 void apagarCobraAntiga(Coordenada[] cobraAntiga);
 
 //Imprime a cobra nova
+//Vitor
 void imprimirCobraNova(Coordenada[] cobraNova);
 
 //Imprime a comida
+//Gabriel
 void imprimirComida(Coordenada comida);
 
 //Imprime o menu com a seta na posicao posSeta
+//Vitor
 void imprimirMenu(int posSeta);
 
 //Imprime a borda na tela
+//Vitor
 void imprimirBorda(TamanhoTela tamanhoTela);
 
 //Mostra o menu e retorna a dificuldade (1, 2 ou 3) ou se vai sair (0)
+//Vitor
 int mostrarMenu();
 
 
